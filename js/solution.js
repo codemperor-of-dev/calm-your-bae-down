00c5a4a9a7e8098b423e8be9c610b9cf87b5a4e7
AungKhantHein <aungkhanthein314@gmail.com>
2022-10-08 18:50:48 +0630

    DaDaDaDahDa

--- js/solution.js
+++ js/solution.js
@@ -1,42 +1,44 @@
-let solutionData = [
-  {
-    id: 1,
-    text: "Your girlfriend is giving you a chance to buy her food, then she will stop being mad at you. Her choice of food is,",
+const solutionData = {
+  1: {
+    text: "Your girlfriend is giving you a chance to buy her food, then she will stop being mad at you. Her choice of food is \"{optional}\"",
     src: "../assests/dating.svg",
   },
-  {
-    id: 2,
-    text: "Your soulmate wants you to ask her out for a movie date, what you should probably do right now. Her choice of movie is,",
+  2: {
+    text: "Your soulmate wants you to ask her out for a movie date, what you should probably do right now. Her choice of movie is \"{optional}\"",
     src: "../assests/solved.svg",
   },
-  {
-    id: 3,
-    text: "Your soulmat is willing to get a love gift from you, she assume you would give her,",
+  3: {
+    text: "Your soulmat is willing to get a love gift from you, she assume you would give her \"{optional}\"",
     src: "../assests/solved.svg",
   },
-  {
-    id: 4,
+  4: {
     text: "Your soulmate is facing a hard time with her family. Listen her, be with her and take great care of her.",
     src: "../assests/dating.svg",
   },
-  {
-    id: 5,
+  5: {
     text: "Your girlfriend is having difficulties dealing with herself, she must have Low self-esteem. So let her know she's enough and take great care of her. ",
     src: "../assests/dating.svg",
   },
-  {
-    id: 6,
+  6: {
     text: "Your girlfriend assumes that you are flirting with other girls, so stop flirting with other girls and start prioritizing her presence.",
     src: "../assests/solved.svg",
   },
-  {
-    id: 7,
-    text: "Your soulmate is havinga feeling that you don't care about her anymore, she feels lonely without your attention around. We suggest you to text her, contant her often.",
+  7: {
+    text: "Your soulmate is having feeling that you don't care about her anymore, she feels lonely without your attention around. We suggest you to text her, contant her often.",
     src: "../assests/solved.svg",
   },
-  {
-    id: 8,
+  8: {
     text: "Sometimes good things just need a break. We advise you should break up for a while and reflect your feelings.",
     src: "../assests/missedChance.svg",
   },
-];
+};
+
+const params = new URLSearchParams(location.search);
+const solutionId = params.get("id");
+const optional = params.get("optional");
+
+const solutionText = document.getElementById("solutionText");
+const solutionImage = document.getElementById("solutionImage");
+
+solutionText.innerText = solutionData[solutionId].text.replace("{optional}", window.atob(optional));
+solutionImage.src = solutionData[solutionId].src;
