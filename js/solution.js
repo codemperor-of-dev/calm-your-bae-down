3fc08cb8598a81ff2d6c489c622cc6abf0c54804
sumyataung2004 <sumyataung1584@gmail.com>
2022-10-08 15:04:40 +0630

    sorry for not writing it on my own :)

---
+++ js/solution.js
@@ -0,0 +1,42 @@
+let solutionData = [
+  {
+    id: 1,
+    text: "Your girlfriend is giving you a chance to buy her food, then she will stop being mad at you. Her choice of food is,",
+    src: "../assests/dating.svg",
+  },
+  {
+    id: 2,
+    text: "Your soulmate wants you to ask her out for a movie date, what you should probably do right now. Her choice of movie is,",
+    src: "../assests/solved.svg",
+  },
+  {
+    id: 3,
+    text: "Your soulmat is willing to get a love gift from you, she assume you would give her,",
+    src: "../assests/solved.svg",
+  },
+  {
+    id: 4,
+    text: "Your soulmate is facing a hard time with her family. Listen her, be with her and take great care of her.",
+    src: "../assests/dating.svg",
+  },
+  {
+    id: 5,
+    text: "Your girlfriend is having difficulties dealing with herself, she must have Low self-esteem. So let her know she's enough and take great care of her. ",
+    src: "../assests/dating.svg",
+  },
+  {
+    id: 6,
+    text: "Your girlfriend assumes that you are flirting with other girls, so stop flirting with other girls and start prioritizing her presence.",
+    src: "../assests/solved.svg",
+  },
+  {
+    id: 7,
+    text: "Your soulmate is havinga feeling that you don't care about her anymore, she feels lonely without your attention around. We suggest you to text her, contant her often.",
+    src: "../assests/solved.svg",
+  },
+  {
+    id: 8,
+    text: "Sometimes good things just need a break. We advise you should break up for a while and reflect your feelings.",
+    src: "../assests/missedChance.svg",
+  },
+];
